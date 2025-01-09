fc83ebc2dc0db57ee1265ed34d13e94d3ae021af
Ali Görkem Aksöz <96394804+AGorkemAksoz@users.noreply.github.com>
2025-01-09 15:39:12 +0300

    U-Initial Commit

---
+++ WordleClone/WordleCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordleCloneApp.swift
+//  WordleClone
+//
+//  Created by Gorkem on 9.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct WordleCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
