1ff599a59eab91ed82e9137fbed7acc41b493e6e
Ali Görkem Aksöz <96394804+AGorkemAksoz@users.noreply.github.com>
2025-01-09 17:43:36 +0300

    keyboard view added

---
+++ WordleClone/Views/LetterButtonView.swift
@@ -0,0 +1,18 @@
+//
+//  LetterButtonView.swift
+//  WordleClone
+//
+//  Created by Gorkem on 9.01.2025.
+//
+
+import SwiftUI
+
+struct LetterButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LetterButtonView()
+}
