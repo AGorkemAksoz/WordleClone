c91b175f85617e53da591228ee61d3e91ceffd79
Ali Görkem Aksöz <a.gorkemaksoz@gmail.com>
2025-01-12 17:49:32 +0300

    toast feedback added and statistics view has done

--- WordleClone/Views/GameView.swift
+++ WordleClone/Views/GameView.swift
@@ -10,53 +10,77 @@ import SwiftUI
 struct GameView: View {
     @EnvironmentObject var dm: WordleDataModel
     var body: some View {
-        NavigationStack {
-            VStack {
-                Spacer()
-                VStack(spacing: 3) {
-                    ForEach(0...5, id: \.self) { index in
-                        GuessView(guess: $dm.guesses[index])
-                            .modifier(Shake(animatableData: CGFloat(dm.incorrectAttemps[index])))
+        ZStack {
+            NavigationStack {
+                VStack {
+                    Spacer()
+                    VStack(spacing: 3) {
+                        ForEach(0...5, id: \.self) { index in
+                            GuessView(guess: $dm.guesses[index])
+                                .modifier(Shake(animatableData: CGFloat(dm.incorrectAttemps[index])))
+                        }
                     }
+                    .frame(width: Global.boardWidth, height: 6 * Global.boardWidth / 5)
+                    Spacer()
+                    Keyboard()
+                        .scaleEffect(Global.keyboardScale)
+                        .padding(.top)
+                    
+                    Spacer()
                 }
-                .frame(width: Global.boardWidth, height: 6 * Global.boardWidth / 5)
-                Spacer()
-                Keyboard()
-                    .scaleEffect(Global.keyboardScale)
-                    .padding(.top)
-                
-                Spacer()
+                    .navigationBarTitleDisplayMode(.inline)
+                    .overlay(alignment: .top) {
+                        if let toastText = dm.toastText {
+                            ToastView(toastText: toastText)
+                                .offset(y: 20)
+                        }
+                    }
+                    .toolbar {
+                        ToolbarItem(placement: .navigationBarLeading) {
+                            HStack {
+                                if !dm.inPlay {
+                                    Button {
+                                        dm.newGame()
+                                    } label: {
+                                        Text("New")
+                                            .foregroundStyle(.primary)
+                                    }
+                                }
+
+                                Button {
+                                    
+                                } label: {
+                                    Image(systemName: "questionmark.circle")
+                                }
+                            }
+                        }
+                        ToolbarItem(placement: .principal) {
+                            Text("WORDLE")
+                                .font(.largeTitle)
+                                .fontWeight(.heavy)
+                                .foregroundStyle(.primary)
+                        }
+                        ToolbarItem(placement: .navigationBarTrailing) {
+                            HStack {
+                                Button {
+                                    withAnimation {
+                                        dm.showStats.toggle()
+                                    }
+                                } label: {
+                                    Image(systemName: "chart.bar")
+                                }
+                                Button {
+                                    
+                                } label: {
+                                    Image(systemName: "gearshape.fill")
+                                }
+                            }
+                        }
+                    }
             }
-                .navigationBarTitleDisplayMode(.inline)
-                .toolbar {
-                    ToolbarItem(placement: .navigationBarLeading) {
-                        Button {
-                            
-                        } label: {
-                            Image(systemName: "questionmark.circle")
-                        }
-                    }
-                    ToolbarItem(placement: .principal) {
-                        Text("WORDLE")
-                            .font(.largeTitle)
-                            .fontWeight(.heavy)
-                            .foregroundStyle(.primary)
-                    }
-                    ToolbarItem(placement: .navigationBarTrailing) {
-                        HStack {
-                            Button {
-                                
-                            } label: {
-                                Image(systemName: "chart.bar")
-                            }
-                            Button {
-                                
-                            } label: {
-                                Image(systemName: "gearshape.fill")
-                            }
-                        }
-                    }
-                }
+            if dm.showStats {
+                StatsView()
+            }
         }
     }
 }
---
+++ WordleClone/Views/StatsView.swift
@@ -0,0 +1,113 @@
+//
+//  StatsView.swift
+//  WordleClone
+//
+//  Created by Ali Görkem Aksöz on 12.01.2025.
+//
+
+import SwiftUI
+
+struct StatsView: View {
+    @EnvironmentObject var dm: WordleDataModel
+    var body: some View {
+        VStack(spacing: 10) {
+            HStack {
+                Spacer()
+                Button {
+                    withAnimation {
+                        dm.showStats.toggle()
+                    }
+                } label: {
+                    Text("X")
+                }
+                .offset(x: 20, y: 10)
+            }
+            Text("STATISTICS")
+                .font(.headline)
+                .fontWeight(.bold)
+            
+            HStack(alignment: .top) {
+                SingleStat(value: dm.currentStat.games,
+                           text: "Played")
+                
+                if dm.currentStat.games > 0 {
+                    SingleStat(value: Int(100 * dm.currentStat.wins/dm.currentStat.games),
+                               text: "Win %")
+                }
+                SingleStat(value: dm.currentStat.streak,
+                           text: "Current Streak")
+                .fixedSize(horizontal: false, vertical: true)
+                
+                SingleStat(value: dm.currentStat.maxStreak,
+                           text: "MAX Streak")
+                .fixedSize(horizontal: false, vertical: true)
+            }
+            Text("GUESS DISTRIBUTION")
+                .font(.headline)
+                .fontWeight(.bold)
+            
+            VStack(spacing: 5) {
+                ForEach(0..<6) { index in
+                    HStack {
+                        Text("\(index + 1)")
+                        if dm.currentStat.frequencies[index] == 0 {
+                            Rectangle()
+                                .fill(Color.wrongColor)
+                                .frame(width: 22, height: 20)
+                                .overlay {
+                                    Text("0")
+                                        .foregroundStyle(.white)
+                                }
+                        } else {
+                            if let maxValue = dm.currentStat.frequencies.max() {
+                                Rectangle()
+                                    .fill((dm.tryIndex == index && dm.isGameOver
+                                           ? Color.correctColor
+                                           : Color.wrongColor))
+                                    .frame(width: CGFloat(dm.currentStat.frequencies[index])
+                                           / CGFloat(maxValue) * 210,
+                                           height: 20)
+                                    .overlay (
+                                        Text("\(dm.currentStat.frequencies[index])")
+                                            .foregroundStyle(.white)
+                                            .padding(.horizontal, 5),
+                                        alignment: .trailing
+                                    )
+                            }
+                        }
+                        Spacer()
+                    }
+                }
+            }
+            Spacer()
+        }
+        .padding(.horizontal, 40)
+        .frame(width: 320, height: 370)
+        .background(RoundedRectangle(cornerRadius: 15)
+                        .fill(Color.systemBackground))
+        .padding()
+        .shadow(color: .black.opacity(0.3), radius: 10)
+        .offset(y: -15)
+    }
+}
+
+#Preview {
+    StatsView()
+        .environmentObject(WordleDataModel())
+}
+
+struct SingleStat: View {
+    let value: Int
+    let text: String
+    
+    var body: some View {
+        VStack {
+            Text("\(value)")
+                .font(.largeTitle)
+            Text(text)
+                .font(.caption)
+                .frame(width: 50)
+                .multilineTextAlignment(.center)
+        }
+    }
+}
